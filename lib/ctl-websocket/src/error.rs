97d6e67d88c7fe19a378e8149061e904e2f232c0
RishiDarkDevil <rishi8001100192@gmail.com>
2026-01-29 03:03:22 +0530

    ctl-websocket: add serde error

--- lib/ctl-websocket/src/error.rs
+++ lib/ctl-websocket/src/error.rs
@@ -3,6 +3,8 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum WebsocketConnectorError {
-    #[error("Websocket error: {0}")]
-    WebsocketConnError(#[from] WebsocketConnError)
+    #[error("websocket connector error: websocket error {0}")]
+    WebsocketConnError(#[from] WebsocketConnError),
+    #[error("websocket connector error: serde json error {0}")]
+    SerdeError(#[from] serde_json::Error),
 }
