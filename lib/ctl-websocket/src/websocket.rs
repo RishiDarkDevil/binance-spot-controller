be9567f137442ce2e25fc8a9168386d97eafe00e
RishiDarkDevil <rishi8001100192@gmail.com>
2026-01-30 03:34:41 +0530

    ctl-websocket: make websocket conn feed kind dependent

--- lib/ctl-websocket/src/websocket.rs
+++ lib/ctl-websocket/src/websocket.rs
@@ -1,30 +1,38 @@
-use atx_feed::{FeedData, FeedPoll, FeedProtocolOps};
+use atx_feed::{FeedData, FeedKind, FeedPoll, FeedProtocolOps, Streams};
 use atx_websocket::{WebsocketConfig, WebsocketConn};
 
 use crate::WebsocketConnectorError;
 
 /// The exchange websocket connector.
 /// This provides all the necessary methods to connect to the exchange websocket.
-pub struct WSConn {
+pub struct WSConn<K: FeedKind> {
     /// The underlying websocket connection.
     websocket: WebsocketConn,
+    /// The streams being subscribed to.
+    streams: Streams<K>,
     /// Buffer for storing received message data.
     recv_buffer: Vec<u8>,
 }
 
-impl WSConn {
+impl<K: FeedKind> WSConn<K> {
     /// Creates a new WSConn instance.
     pub fn new(url: &str) -> Result<Self, WebsocketConnectorError> {
         let mut websocket = WebsocketConn::new(url, WebsocketConfig::default())?;
         websocket.connect()?;
         Ok(Self {
             websocket,
+            streams: Streams::new(),
             recv_buffer: Vec::with_capacity(4096),
         })
     }
+
+    /// Returns a reference to the subscribed streams.
+    pub fn streams(&self) -> &Streams<K> {
+        &self.streams
+    }
 }
 
-impl FeedProtocolOps for WSConn {
+impl<K: FeedKind> FeedProtocolOps for WSConn<K> {
     type FeedProtocolError = WebsocketConnectorError;
 
     fn poll(&mut self) -> Result<FeedPoll<'_>, Self::FeedProtocolError> {
